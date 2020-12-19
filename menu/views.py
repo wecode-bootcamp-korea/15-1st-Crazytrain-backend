59463c89478626b53891c812299084995621f89d
dltngks54 <dltngks54@naver.com>
2020-12-19 14:43:51 +0900

    스토어 사이드바 api 작성

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+from menu.models import Category, SubCategory
+
+class CategoryView(View):
+    def get(self, request):
+         try:
+            categories = Category.objects.prefetch_related('subcategory_set').all()
+
+            if categories.exists():   
+                categories = [{ 
+                 'categoryId'              :  category.id,
+                 'category'           :  category.name,
+                 'subCategories' : [
+                     sub_category.name
+                for sub_category in category.subcategory_set.all()]
+            } for category in categories]
+             
+            return JsonResponse({"categories" : categories}, status=200)
+            
+         except ValueError:
+             return JsonResponse({'message':'VALUE_ERROR'}, status=400)
+        
+         except:
+             return JsonResponse({'message':'ERROR'}, status=400)
+
+  
+    
