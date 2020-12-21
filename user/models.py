2c1e2cb0f6d25222299f5865b3c3dabf75e6dae0
mincheolkk <kmc04080408@gmail.com>
2020-12-21 10:18:58 +0900

    models.py 다시 수정

--- user/models.py
+++ user/models.py
@@ -2,24 +2,25 @@ from django.db import models
 from product.models import Product
 
 class Gender(models.Model):
-    gender = models.PositiveSmallIntegerField(default=0, null=True)
+    name = models.CharField(max_length=1, null=True)
 
     class Meta:
         db_table = 'genders'
+
 class User(models.Model):
     email         = models.EmailField(max_length=100, unique=True)
     password      = models.CharField(max_length=100)
     nickname      = models.CharField(max_length=15, unique=True)
     created_at    = models.DateTimeField(auto_now_add=True)
     updated_at    = models.DateTimeField(auto_now=True)
-    gender_id     = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    gender_id     = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True)
     birth_date    = models.DateField(auto_now=False, null=True)
     profile_image = models.URLField(max_length=256,null=True,default=‘https://image.ohou.se/i/bucketplace-v2-development/uploads/default_images/avatar.png?gif=1&amp;w=640&amp;h=640&amp;c=c&amp;webp=1’)
     note          = models.CharField(max_length=50, null=True)
 
     class Meta:
         db_table = 'users'
-
+    
     def __str__(self):
         return self.email
 
@@ -27,5 +28,6 @@ class History(models.Model):
     user       = models.ForeignKey(User, on_delete=models.CASCADE)
     product    = models.ForeignKey(Product, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         db_table = 'histories'
