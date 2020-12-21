eba987d3cafb640ed44661bc2f576e3cdd9018d0
YEOJU SEOK <seok@YEOJUui-MacBookPro.local>
2020-12-21 20:00:10 +0900

    models.py수정본

---
+++ shoppingcart/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from user.models import User
+from prodect.models import Product, OptionSize, OptionColor
+
+class Cart(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    product    = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    quantity   = models.CharField(max_length=256, default=1)
+    size       = models.ForeignKey(OptionSize, on_delete=models.CASCADE, null=True)
+    color      = models.ForeignKey(OptionColor, on_delete=models.CASCADE, null=True)
+
+
+    class Meta:
+        db_table = 'carts'
+
--- user/models.py
+++ user/models.py
@@ -24,10 +24,3 @@ class User(models.Model):
     def __str__(self):
         return self.email
 
-class History(models.Model):
-    user       = models.ForeignKey(User, on_delete=models.CASCADE)
-    product    = models.ForeignKey(Product, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = 'histories'
