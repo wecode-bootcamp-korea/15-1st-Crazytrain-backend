98df59d6dc29ddf72ab5bc851486bcb271cd0736
YEOJU SEOK <seok@YEOJUui-MacBookPro.local>
2020-12-24 02:29:29 +0900

    임시

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,5 @@ from .views      import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
-    path('/<int:product_id>', ProductDetailView.as_view()),
+    path('/<int:package_id>', ProductDetailView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -18,36 +18,45 @@ class ProductListView(View):
             'option_set').select_related(
             'sub_category').filter(
             sub_category_id=sub_category_id)
-        
+#[image.url for image in product.productimage_set.all()],
         context = [{
-            'id'            : product.package.id,
-            'alt'           : product.package.name,
-            'images'        : [image.url for image in product.productimage_set.all()],
-            'articleName'   : product.package.name,
-            'brandName'     : product.brand.name,
-            'ispackage'     : "true",
-            'itemBadge'     : "",
-            'options'       : [{'color':option.option_color.name,'price':int(option.price)} for option in product.option_set.all()]
+            'id'           : product.package.id,
+            'alt'          : product.package.name,
+            'product_image': [image.url for image in product.productimage_set.all()],
+            'articleName'  : product.package.name,
+            'brandName'    : product.brand.name,
+            'ispackage'    : "true",
+            'itemBadge'    : product.sale.name,
+            'options'      : [{'color':option.option_color.name,'price':int(option.price)
+                            } for option in product.option_set.all()]
         }for product in products]
-
         return JsonResponse({"productlist": context}, status=200)
-
 class ProductDetailView(View):
      def get(self, request, package_id):
-         try:
-            packages = Package.objects.prefetch_related('product_set').get(id=package_id)
+        try:
+            Image.objects.filter(product=product.id)
+            products = Product.objects.prefetch_related(
+            'productimage_set',
+            'option_set').select_related(
+            'sub_category').filter(
+            package_id=package_id)
             context = [{
-                '상품제목': product.package.name,
-                '상품이름': product.name,
-                '상품id' : product.id,
-                '가격'   : int(str(option.price).split(".")[0]),
-                '옵션id' : option.id,
-                '옵션색깔': option.option_color.name,
-                '상품사진': image.url
-            } for product in packages.product_set.all() 
-            for option in Option.objects.filter(product=product.id) for image in ProductImage.objects.filter(product=product.id)]
+                'id'           : product.id,
+                'productName'  : product.name,
+                'product_image': [image.url for image in product.productimage_set.all()],
+                'brandName'    : product.brand.name,
+                'articleName'  : product.package.name,
+                'alt'          : product.package.name,
+                'sub_category' : product.sub_category.name,
+                'options'      : [{'color':option.option_color.name,'price':int(option.price)
+                                 } for option in product.option_set.all()],
+                'ispackage'    : "true",
+                'itemBadge'    : product.sale.name,
+                'infoImage'    : product.information_image,
+                'category'     : Category.objects.get(id=1).name
+            }for product in products]
             return JsonResponse({'productdetail' :context}, status=200)
-         except Product.DoesNotExist:
+        except Product.DoesNotExist:
              return JsonResponse({'MESSAGE':'NO_PRODUCT'}, status=404)
-         except ValueError:
-             return JsonResponse({'message':'VALUE_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'message':'VALUE_ERROR'}, status=400)
